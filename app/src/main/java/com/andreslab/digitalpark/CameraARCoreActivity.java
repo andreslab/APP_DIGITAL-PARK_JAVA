fb60c314af57b4d396b87d638090dd925b44bc81
jaime andrade <jaimeandrade454@gmail.com>
2019-08-08 15:03:12 -0500

    render 3dmodel

---
+++ app/src/main/java/com/andreslab/digitalpark/CameraARCoreActivity.java
@@ -0,0 +1,71 @@
+package com.andreslab.digitalpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class CameraARCoreActivity extends AppCompatActivity {
+
+    ArFragment arFragment;
+    private ModelRenderable animalRenderable;
+    ImageView imgAnimal;
+    TextView titleAnimal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_camera_arcore);
+
+        setupModel();
+
+        arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.sceneform_ux_fragment);
+        imgAnimal = (ImageView)findViewById(R.id.imgAnimal);
+        titleAnimal = (TextView) findViewById(R.id.titleAnimal);
+
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                createModel(anchorNode);
+            }
+        });
+
+    }
+
+    private void createModel (AnchorNode anchorNode){
+        TransformableNode animal = new TransformableNode(arFragment.getTransformationSystem());
+        animal.setParent(anchorNode);
+        animal.setRenderable(animalRenderable);
+        animal.select();
+
+
+    }
+
+    private void setupModel(){
+        ModelRenderable.builder()
+                .setSource(this, R.raw.sphere3)
+                .build().thenAccept(renderable -> animalRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "No se pudo cargar el modelo", Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+                );
+    }
+}
--- app/src/main/java/com/andreslab/digitalpark/MainActivity.java
+++ app/src/main/java/com/andreslab/digitalpark/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_mamifero;
     Button btn_ave;
     Button btn_reptil;
+    ImageButton btn_camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         btn_mamifero = findViewById(R.id.btnMamifero);
         btn_ave = findViewById(R.id.btnAves);
         btn_reptil = findViewById(R.id.btnReptiles);
+        btn_camera = findViewById(R.id.btnCamera);
 
         btn_mamifero.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), CategoryActivity.class);
                 i.putExtra("category", 2);
+                startActivity(i);
+            }
+        });
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), CameraARCoreActivity.class);
                 startActivity(i);
             }
         });
