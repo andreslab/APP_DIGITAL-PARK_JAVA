f7a6f9e6f135e6c8272a8a72f8f982bc1ea29595
Jaime Andrade <jaimeandrade454@gmail.com>
2019-09-07 08:47:09 -0500

    fix style popup and add button camera, close

--- app/src/main/java/com/andreslab/digitalpark/PopUpActivity.java
+++ app/src/main/java/com/andreslab/digitalpark/PopUpActivity.java
@@ -1,19 +1,87 @@
 package com.andreslab.digitalpark;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.display.DisplayManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PopUpActivity extends Activity {
+
+    private static final String TAG = "PopUp";
+    String animal_name = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up);
+
+        TextView title = (TextView)findViewById(R.id.title_detail_animal);
+        TextView detail = (TextView)findViewById(R.id.text_detail_animal);
+        ImageView img_animal = (ImageView)findViewById(R.id.img_detail_animal);
+        ImageButton btnClose = (ImageButton)findViewById(R.id.btn_close);
+        ImageButton btnCamera = (ImageButton)findViewById(R.id.btn_camera);
+
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null) {
+            animal_name = extras.getString("name");
+        }else{
+            Toast.makeText(getApplicationContext(), "A ocurrido un problema", Toast.LENGTH_SHORT).show();
+        }
+
+
+        if (animal_name != "") {
+
+            title.setText(animal_name);
+
+            int idImg = getResources().getIdentifier("img_"+animal_name+"_min", "drawable", getPackageName());
+            Log.i(TAG, "imagen: "+"img_"+animal_name+"  codigo:  "+idImg);
+            if (idImg != 0) {
+                img_animal.setImageResource(idImg);
+            }
+
+
+            int idDescription = getResources().getIdentifier("description_"+animal_name, "string", getPackageName());
+            Log.i(TAG, "Descripcion: "+"description_"+animal_name+"  codigo:  "+idDescription);
+            if(idDescription != 0){
+                String textDetail = getResources().getString(idDescription);
+                detail.setText(textDetail);
+            }
+        }
+
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CameraActivity.class);
+                i.putExtra("name", animal_name);
+                startActivity(i);
+            }
+        });
+
+
+        //config style
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -22,6 +90,8 @@ public class PopUpActivity extends Activity {
         int width = dm.widthPixels;
 
         getWindow().setLayout((int) (width * .8), (int) (heigth* .7));
+
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
